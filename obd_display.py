95877c75064a766c1b4e449dd0dcf089a0fb46fd
chan2565 <1981757+chan2565@users.noreply.github.com>
2019-11-28 12:47:45 -0500

    Initial display with OBD commands that match the hardware

--- obd_display.py
+++ obd_display.py
@@ -2,40 +2,52 @@ import obd, time
 from obd_gui import *
 
 # Methods for updating stats
-def new_rpm(raw_rpm):
-	rpm = str(raw_rpm).split('.')[0]
-	rpm_int = int(rpm)
-	rpm_lbl.configure(text=rpm + " RPM")
-	bar_num = int((rpm_int / 2500) * 100)
-	rpm_bar['value'] = bar_num
-	if rpm_int >= 2500:
-		rpm_bar['style'] = 'red.Horizontal.TProgressbar'
-		shift_lbl.configure(text="SHIFT")
-	if rpm_int < 2500:
-		rpm_bar['style'] = 'green.Horizontal.TProgressbar'
-		shift_lbl.configure(text="")
+# Column 0
 def new_speed(raw_speed):
 	kph = str(raw_speed).split(' ')[0]
 	mph = str(int(float(kph) * 0.6214))
 	mph_lbl.configure(text=mph + " MPH")
-def new_air_status(raw_air_status):
-	air_status = str(raw_air_status)
-	air_status_lbl.configure(text=air_status)
-def new_fuel_rate(raw_fuel_rate):
-	fuel_rate = "Fuel Rate\n" + str(raw_fuel_rate)
-	fuel_rate_lbl.configure(text=fuel_rate)
-def new_oil_temp(raw_oil_temp):
-	oil_temp = "Oil Temp.\n" + str(raw_oil_temp)
-	oil_temp_lbl.configure(text=oil_temp)
-def new_fuel_lvl(raw_fuel_lvl):
-	fuel_lvl = str(raw_fuel_lvl)
-	fuel_lvl_lbl.configure(text=fuel_lvl)
-def new_fuel_type(raw_fuel_type):
-	fuel_type = str(raw_fuel_type)
-	fuel_type_lbl.configure(text=fuel_type)
+def new_rpm(raw_rpm):
+	rpm = str(raw_rpm).split('.')[0]
+	rpm_int = int(rpm)
+	rpm_lbl.configure(text=rpm + " RPM")
+	bar_num = int((rpm_int / 3000) * 100)
+	rpm_bar['value'] = bar_num
+	if rpm_int >= 3000:
+		rpm_bar['style'] = 'red.Horizontal.TProgressbar'
+		shift_lbl.configure(text="SHIFT", bg="red")
+	if rpm_int < 3000:
+		rpm_bar['style'] = 'green.Horizontal.TProgressbar'
+		shift_lbl.configure(text="", bg="")
+def new_obd_voltage(raw_obd_voltage):
+	obd_voltage_status = str(raw_obd_voltage)
+	voltage_lbl.configure(text=obd_voltage_status)
 def new_fuel_status(raw_fuel_status):
 	fuel_status = str(raw_fuel_status)
 	fuel_status_lbl.configure(text=fuel_status)
+# Column 1
+def new_coolant_temp(raw_coolant_temp):
+	coolant_temp = "Coolant Temp.\n" + str(raw_coolant_temp)
+	coolant_temp_lbl.configure(text=coolant_temp)
+def new_engine_load(raw_engine_load):
+	load = str(raw_engine_load).split('.')[0]
+	load_int = int(load)
+	engine_load = str(raw_engine_load) + " Load"
+	engine_load_lbl.configure(text=engine_load)
+	engine_load_bar['value'] = load_int
+	if load_int >= 85:
+		rpm_bar['style'] = 'red.Horizontal.TProgressbar'
+	if load_int < 85:
+		rpm_bar['style'] = 'green.Horizontal.TProgressbar'
+def new_intake_temp(raw_intake_temp):
+	intake_temp = str(raw_intake_temp)
+	air_temp_lbl.configure(text=intake_temp)
+def new_throttle_pos(raw_throttle_pos):
+	throttle_pos = str(raw_throttle_pos)
+	throttle_pos_lbl.configure(text=throttle_pos)
+def new_timing_adv(raw_timing_adv):
+	timing_adv = str(raw_timing_adv)
+	timing_adv_lbl.configure(text=timing_adv)
 
 try:
 	# Start async connection to OBD adapter
@@ -43,14 +55,17 @@ try:
 	connection = obd.Async()
 
 	# Set up codes to watch with callbacks
-	connection.watch(obd.commands.RPM, callback=new_rpm)
+	# Column 0
 	connection.watch(obd.commands.SPEED, callback=new_speed)
-	#connection.watch(obd.commands.AIR_STATUS, callback=new_air_status)
-	#connection.watch(obd.commands.FUEL_RATE, callback=new_fuel_rate)
-	#connection.watch(obd.commands.OIL_TEMP, callback=new_oil_temp)
-	#connection.watch(obd.commands.FUEL_LEVEL, callback=new_fuel_lvl)
-	#connection.watch(obd.commands.FUEL_TYPE, callback=new_fuel_type)
-	#connection.watch(obd.commands.FUEL_STATUS, callback=new_fuel_status)
+	connection.watch(obd.commands.RPM, callback=new_rpm)
+	connection.watch(obd.commands.ELM_VOLTAGE, callback=new_obd_voltage)
+	connection.watch(obd.commands.FUEL_STATUS, callback=new_fuel_status)
+	# Column 1
+	connection.watch(obd.commands.COOLANT_TEMP, callback=new_coolant_temp)
+	connection.watch(obd.commands.ENGINE_LOAD, callback=new_engine_load)
+	connection.watch(obd.commands.INTAKE_TEMP, callback=new_intake_temp)
+	connection.watch(obd.commands.THROTTLE_POS, callback=new_throttle_pos)
+	connection.watch(obd.commands.TIMING_ADVANCE, callback=new_timing_adv)
 
 	# Start monitoring
 	connection.start()
--- obd_gui.py
+++ obd_gui.py
@@ -31,47 +31,51 @@ rpm_lbl = Label(window, text="0000 RPM", font=('Courier New', 48), padx=5, pady=
 rpm_lbl.grid(column=0, row=2, sticky="nsew")
 
 # RPM bar
-rpm_bar = ttk.Progressbar(window, length=250, style='red.Horizontal.TProgressbar')
+rpm_bar = ttk.Progressbar(window, length=250, style='green.Horizontal.TProgressbar')
 rpm_bar['value'] = 10
 rpm_bar.grid(column=0, row=3, sticky="nsew")
 
 # Shift label
-shift_lbl = Label(window, text="--", font=('Courier New', 14), fg='white', bg='red')
+shift_lbl = Label(window, text="--", font=('Courier New', 14), fg='black')
 shift_lbl.grid(column=0, row=4, sticky="nsew")
 
-# Air status label
-air_status_lbl = Label(window, text="Air Status: Everything Nominal", font=('Courier New', 14))
-air_status_lbl.grid(column=0, row=6, sticky="nsew")
+# OBD voltage label
+voltage_lbl = Label(window, text="00.00 V", font=('Courier New', 14), fg='black')
+voltage_lbl.grid(column=0, row=5, sticky="nsew")
+
+# Fuel status label
+fuel_status_lbl = Label(window, text="Fuel Status: --", font=('Courier New', 14))
+fuel_status_lbl.grid(column=0, row=6, sticky="nsew")
 
 ## Column 1
 # Quit button
 quit_btn = Button(window, text="Quit", font=('Courier New', 14), fg='black', bg='red', command=window.destroy)
 quit_btn.grid(column=1, row=0, sticky="e")
 
-# Fuel rate label
-fuel_rate_lbl = Label(window, text="Fuel Rate\n.23 L/h", font=('Courier New', 32), padx=5, pady=5)
-fuel_rate_lbl.grid(column=1, row=1, sticky="nsew")
+# Coolant temperature label
+coolant_temp_lbl = Label(window, text="Coolant Temp.\n--- C", font=('Courier New', 32), padx=5, pady=5)
+coolant_temp_lbl.grid(column=1, row=1, sticky="nsew")
 
-# Oil temperature label
-oil_temp_lbl = Label(window, text="Oil Temp.\n100 C", font=('Courier New', 24), padx=5, pady=5)
-oil_temp_lbl.grid(column=1, row=2, sticky="nsew")
+# Engine load label
+engine_load_lbl = Label(window, text="000% Load", font=('Courier New', 48), padx=5, pady=5)
+engine_load_lbl.grid(column=1, row=2, sticky="nsew")
 
-# Fuel level bar
-fuel_lvl_bar = ttk.Progressbar(window, length=250, style='green.Horizontal.TProgressbar')
-fuel_lvl_bar['value'] = 10
-fuel_lvl_bar.grid(column=1, row=3, sticky="nsew")
+# Engine load bar
+engine_load_bar = ttk.Progressbar(window, length=250, style='green.Horizontal.TProgressbar')
+engine_load_bar['value'] = 10
+engine_load_bar.grid(column=1, row=3, sticky="nsew")
 
-# Fuel level label
-fuel_lvl_lbl = Label(window, text="Fuel: ---%", font=('Courier New', 14))
-fuel_lvl_lbl.grid(column=1, row=4, sticky="nsew")
+# Intake air temperature label
+air_temp_lbl = Label(window, text="Intake Air Temp.\n--- C", font=('Courier New', 14), padx=5, pady=5)
+air_temp_lbl.grid(column=1, row=4, sticky="nsew")
 
-# Fuel type label
-fuel_type_lbl = Label(window, text="Fuel Type: Premium etc.", font=('Courier New', 14))
-fuel_type_lbl.grid(column=1, row=5, sticky="nsew")
+# Throttle position label
+throttle_pos_lbl = Label(window, text="Throttle Position: --", font=('Courier New', 14))
+throttle_pos_lbl.grid(column=1, row=5, sticky="nsew")
 
-# Fuel status label
-fuel_status_lbl = Label(window, text="Fuel Status: Everything Nominal", font=('Courier New', 14))
-fuel_status_lbl.grid(column=1, row=6, sticky="nsew")
+# Timing advance label
+timing_adv_lbl = Label(window, text="Timing Advance: --", font=('Courier New', 14))
+timing_adv_lbl.grid(column=1, row=6, sticky="nsew")
 
 # Set weights
 window.columnconfigure(0, weight=1)
